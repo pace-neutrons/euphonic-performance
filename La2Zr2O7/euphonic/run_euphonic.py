3fa722651e10e9b617e74840c3c7d9e8f6905225
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-10-30 13:45:42 +0000

    Add Euphonic batch script and cProfile reading

---
+++ La2Zr2O7/euphonic/run_euphonic.py
@@ -0,0 +1,51 @@
+from euphonic import ureg, ForceConstants
+from timeit import default_timer as timer
+import numpy as np
+import seekpath
+import sys
+
+def start_timer(profile):
+    if profile:
+        return timer()
+    else:
+        return None
+
+def end_timer(profile_data, funcstr, start_time):
+    end = timer()
+    if start_time is not None:
+        profile_data.append(funcstr + f' {end-start: .3f}\n')
+
+interpolate_kwargs = {'use_c': False}
+if len(sys.argv) > 1 and int(sys.argv[1]) > 0:
+    interpolate_kwargs = {'use_c': True,
+                          'n_threads': int(sys.argv[1]),
+                          'fall_back_on_python': False}
+
+if len(sys.argv) > 2:
+    profile = True
+    profile_out = sys.argv[2]
+else:
+    profile = False
+profile_data = []
+
+fc = ForceConstants.from_castep('../../castep/La2Zr2O7.castep_bin')
+qpts = np.loadtxt('../../../qpts_25000.txt')
+#qpts = qpts[:250]
+
+start = start_timer(profile)
+phon = fc.calculate_qpoint_phonon_modes(
+qpts, asr='reciprocal', reduce_qpts=False, **interpolate_kwargs)
+end_timer(profile_data, 'euphonic calculate_qpoint_phonon_modes', start)
+
+start = start_timer(profile)
+sf = phon.calculate_structure_factor()
+end_timer(profile_data, 'euphonic calculate_structure_factor', start)
+
+ebins = np.arange(200)*ureg('meV')
+start = start_timer(profile)
+sqw = sf.calculate_sqw_map(ebins)
+end_timer(profile_data, 'euphonic calculate_sqw_map', start)
+
+if profile:        
+    with open(profile_out, 'w') as f:
+        f.writelines(profile_data)
--- read_profile.py
+++ read_profile.py
@@ -1,67 +1,134 @@
 import os
 from typing import List, Optional, Union, Tuple
+import warnings
 
 import numpy as np
 from numpy import ndarray
 from numpy.ma.core import MaskedArray
 
-def get_castep_dir(material: str, nproc: int) -> str:
-    """Get name of directory containing CASTEP .profile files"""
-    direc = os.path.join(material, 'castep', 'np' + str(nproc))
+
+def get_dir(material: str, code: str, nproc: int) -> str:
+    """Get name of directory containing profile files"""
+    direc = os.path.join(material, code, 'np' + str(nproc))
     return direc
+
 
 def get_castep_fname(material: str, proc_num: int) -> str:
     """Get name of CASTEP .profile file"""
     return f'{material}.{proc_num:04d}.profile'
 
+
 def get_castep_fnames(material: str, nproc: int) -> List[str]:
     """Get path and name of all CASTEP .profile files in directory"""
-    path = get_castep_dir(material, nproc)
+    path = get_dir(material, 'castep', nproc)
     fnames = []
     for p in range(1, nproc + 1):
         fnames.append(
             os.path.join(path, get_castep_fname(material, p)))
     return fnames
 
-def get_castep_func_prof_from_file(
-        file_name: str, function_name: str, 
+
+def get_cprofile_fname(material: str, nproc: int) -> str:
+    """Get name of output cProfile file"""
+    return f'{material}_np{nproc}_cprofile.txt'
+
+
+def get_timeit_fname(material: str, nproc: int) -> str:
+    """Get name of custom output timeit file"""
+    return f'{material}_np{nproc}_timeit.txt'
+
+
+def get_func_prof_from_file(
+        file_name: str, function_name: str,
+        file_type: Optional[str] = 'castep',
         ignore_missing: Optional[bool] = True
-        ) -> Union[Tuple[None, None, None],
-                   Tuple[ndarray, ndarray, ndarray]]:
+        ) -> Union[Tuple[None, None],
+                   Tuple[ndarray, ndarray]]:
     """
     Read profiling information for a particular function from a
-    particular .profile file. If ignore_missing=False, raise an
+    particular profiling file. If ignore_missing=False, raise an
     error if the function information can't be found.
     """
     print(f'Reading {file_name}')
     with open(file_name, 'r') as f:
         data = f.readlines()
-    function_name_found = False
+    if file_type == 'castep':
+        timing, n_calls = extract_castep_func_prof(
+            data, function_name, file_name)
+    elif file_type == 'cprofile':
+        timing, n_calls = extract_cprofile_func_prof(data, function_name)
+    elif file_type == 'timeit':
+        timing, n_calls = extract_timeit_func_prof(data, function_name)
+    else:
+        raise ValueError(f'File type {file_type} not recognised')
+    if timing is None and not ignore_missing:
+        raise ValueError(f'{function_name} not found in {file_name}')
+    return timing, n_calls
+
+
+def extract_castep_func_prof(data: List[str], func_name: str, file_name: str):
+    """
+    Read profiling information for a particular function from a
+    CASTEP .profile file
+    """
+    n_calls = None
+    timing = None
     for line in data:
-        if ' ' + function_name + ' ' in line:
+        if ' ' + func_name + ' ' in line:
+            print(line)
             split_line = line.split()
             try:
-                time = float(split_line[-2].strip('s')) 
+                timing = float(split_line[-2].strip('s'))
+                n_calls = int(split_line[-4])
                 n_profiled_calls = int(split_line[-3]) 
-                n_calls = int(split_line[-4]) 
-                function_name_found = True
-                print(line)
+                if n_calls != n_profiled_calls:
+                    warnings.warn((
+                        f'Number of calls is not the same as number of '
+                        f'profiled calls for {func_name} in {file_name}'))
                 break
             except ValueError:
                 pass
-    if not function_name_found:
-        if ignore_missing:
-            return None, None, None
-        else:
-            raise ValueError(f'{function_name} not found in {file_name}')
-           
-    return time, n_profiled_calls, n_calls
+    return timing, n_calls
+
+
+def extract_cprofile_func_prof(data: List[str], func_name: str):
+    """
+    Read profiling information for a particular function from a
+    cProfile output file
+    """
+    n_calls = None
+    timing = None
+    for line in data:
+        if func_name in line:
+            print(line)
+            split_line = line.split()
+            n_calls = int(split_line[0])
+            timing = float(split_line[3])  # cumulative time
+            break
+    return timing, n_calls
+
+
+def extract_timeit_func_prof(data: List[str], func_name: str):
+    """
+    Read profiling information for a particular function for
+    timeit data
+    """
+    n_calls = 1
+    timing = None
+    for line in data:
+        if func_name in line:
+            print(line)
+            split_line = line.split()
+            timing = float(split_line[-1])
+            break
+    return timing, n_calls
+
 
 def get_castep_parallel_func_prof(
         material: str, nproc: int, function_name: str,
         ignore_missing: Optional[bool] = True
-        ) -> Union[Tuple[ndarray, ndarray, ndarray],
-                   Tuple[MaskedArray, MaskedArray, MaskedArray]]:
+        ) -> Union[Tuple[ndarray, ndarray],
+                   Tuple[MaskedArray, MaskedArray]]:
     """
     Read profiling information for a particular function from all
     .profile files within a directory. If some files don't contain a
@@ -70,42 +137,66 @@ def get_castep_parallel_func_prof(
     """
     fnames = get_castep_fnames(material, nproc)
     times = np.full(len(fnames), -1, dtype=np.float64)
-    n_prof_calls = np.full(len(fnames), -1, dtype=np.int32)
     n_calls = np.full(len(fnames), -1, dtype=np.int32)
     for i, fname in enumerate(fnames):
-        times_i, n_prof_calls_i, n_calls_i = get_castep_func_prof_from_file(
-            fname, function_name, ignore_missing=ignore_missing)
+        times_i, n_calls_i = get_func_prof_from_file(
+            fname, function_name, file_type='castep',
+            ignore_missing=ignore_missing)
         if times_i is None:
             if not isinstance(times_i, np.ma.MaskedArray):
                 times = np.ma.array(times)
-                n_prof_calls = np.ma.array(n_prof_calls)
                 n_calls = np.ma.array(n_calls)
-            times_i, n_prof_calls_i, n_calls_i = (np.ma.masked, np.ma.masked,
-                                                  np.ma.masked)
+            times_i, n_calls_i = (np.ma.masked, np.ma.masked)
         times[i] = times_i
-        n_prof_calls[i] = n_prof_calls_i
         n_calls[i] = n_calls_i
-    return times, n_prof_calls, n_calls
+    return times, n_calls
+
 
 def get_castep_all_func_prof(
-        materials: List, nprocs: List, function_name: str,
+        materials: List[str], nprocs: List[int], function_name: str,
         ignore_missing: Optional[bool] = True
-        ) -> Union[Tuple[ndarray, ndarray, ndarray],
-                   Tuple[MaskedArray, MaskedArray, MaskedArray]]:
+        ) -> Union[Tuple[ndarray, ndarray],
+                   Tuple[MaskedArray, MaskedArray]]:
     """
-    Read profiling information for a particular function from
+    Read CASTEP profiling information for a particular function from
     all materials/nprocessor directories
     """
     times = np.empty((len(materials), len(nprocs)), dtype=object)
-    n_prof_calls = np.empty((len(materials), len(nprocs)), dtype=object)
     n_calls = np.empty((len(materials), len(nprocs)), dtype=object)
     for i, mat in enumerate(materials):
         for j, proc in enumerate(nprocs):
-            (times[i, j], n_prof_calls[i, j],
-             n_calls[i, j]) = get_castep_parallel_func_prof(
-                 mat, proc, function_name)
-    return times, n_prof_calls, n_calls
+            (times[i, j], n_calls[i, j]) = get_castep_parallel_func_prof(
+                mat, proc, function_name)
+    return times, n_calls
+
+
+def get_euphonic_all_func_prof(
+        materials: List[str], nprocs: List[int], function_name: str,
+        file_type: Optional[str] = 'cprofile',
+        ignore_missing: Optional[bool] = False
+        ) -> Union[Tuple[ndarray, ndarray],
+                   Tuple[MaskedArray, MaskedArray]]:
+    """
+    Read Euphonic profiling information for a particular function from
+    all materials/nprocessor directories
+    """
+    times = np.full((len(materials), len(nprocs)), -1, dtype=np.float64)
+    n_calls = np.full((len(materials), len(nprocs)), -1, dtype=np.int32)
+    for i, mat in enumerate(materials):
+        for j, proc in enumerate(nprocs):
+            if file_type == 'cprofile':
+                fname = get_cprofile_fname(mat, proc)
+            elif file_type=='timeit':
+                fname = get_timeit_fname(mat, proc)
+            else:
+                raise ValueError(f'File type {file_type} not recognised')
+            fname = os.path.join(get_dir(mat, 'euphonic', proc), fname)
+            times[i, j], n_calls[i, j] = get_func_prof_from_file(
+                fname, function_name, file_type=file_type,
+                ignore_missing=ignore_missing)
+    return times, n_calls
+
 
 materials = ['La2Zr2O7', 'quartz', 'Nb-181818-s0.5-NCP19-vib-disp']
 nprocs = [1, 2 ,4, 8, 12, 16, 24]
-times, npc, nc = get_castep_all_func_prof(materials, nprocs, 'castep') 
+times, nc = get_castep_all_func_prof(materials, nprocs, 'castep')
